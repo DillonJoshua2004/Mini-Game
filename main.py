43d3047ffbcefe12ffe9176fa0c53df80cb469cd
DillonJoshua2004 <dillon.liyanage@hotmail.com>
2022-12-23 15:58:20 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import time, os
+import random as rndm
+# from characters import * -- to access the code for the characters
+
+# before editing the program read the notes.txt file
+
+inventory = []
+	
+# basic loop to start the game
+while True:
+	print("Initialising Game")
+	time.sleep(1)
+	os.system("clear")
+	
